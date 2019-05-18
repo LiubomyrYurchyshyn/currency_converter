70a0a90fb59fca5465b96d02eecb19cae7fbe60d
LiubomyrYurchyshyn <42535795+LiubomyrYurchyshyn@users.noreply.github.com>
2019-05-18 19:37:06 +0300

    1

---
+++ precache-manifest.29e4f8ccdb7fb52e19f82f48afb2eb39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d500218d6e8a8272ab6c7131022ce4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97927bc8b91cc6875ce9",
+    "url": "/static/css/2.1ad4406c.chunk.css"
+  },
+  {
+    "revision": "3dd4f529ae0d71f597c1",
+    "url": "/static/css/main.5a667a2a.chunk.css"
+  },
+  {
+    "revision": "97927bc8b91cc6875ce9",
+    "url": "/static/js/2.ac33b26c.chunk.js"
+  },
+  {
+    "revision": "3dd4f529ae0d71f597c1",
+    "url": "/static/js/main.99b28f9f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
